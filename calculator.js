751f69dbc3dbadab326c06b67b6b4a425ee67e3e
sushant jadhav <sushantjadhav111@gmail.com>
2024-10-26 11:42:58 +0530

    validation for negative no

--- calculator.js
+++ calculator.js
@@ -3,11 +3,18 @@ const stringCalculation=(str)=>{
 const arr=str.split(',')
 
 const sum=arr.reduce((accumalator,item)=>{
+    if(Number(item)<=0){return new Error("negative numbers not allowed"+item)}
     return accumalator+=Number(item);
 },0)
 
+return sum
 
 }
 
 stringCalculation("")
+stringCalculation("1")
+stringCalculation("1,2,3")
+console.log(stringCalculation("1,2,3"))
+
+
 
