29b4c56f67b115ed06d0264d07d2a2621f828a35
sushant jadhav <sushantjadhav111@gmail.com>
2024-10-26 12:29:43 +0530

    Refactored code, added validation and testcases

--- calculator.js
+++ calculator.js
@@ -1,20 +1,28 @@
 // String calculation function
-const stringCalculation=(str)=>{
-const arr=str.split(',')
+const add=(str)=>{
+str=str.replaceAll("//","")
+str=str.replaceAll(';',",")
+str=str.replaceAll('\n',"")
+const arr  = str.split(",")
 
-const sum=arr.reduce((accumalator,item)=>{
-    if(Number(item)<=0){return new Error("negative numbers not allowed"+item)}
-    return accumalator+=Number(item);
+let negativeStr=''
+
+const sum= arr.reduce((accumalator,ele)=>{
+    let item=ele;
+    if(Number(item) < 0 ){
+        negativeStr = negativeStr + item.toString()+",";
+        return new Error("negative numbers not allowed" + (negativeStr));
+    }
+    return accumalator += Number(item);
 },0)
-
 return sum
-
 }
 
-stringCalculation("")
-stringCalculation("1")
-stringCalculation("1,2,3")
-console.log(stringCalculation("1,2,3"))
+// TestCases
+//  add("") // Output: 0
+//  add("-1,-4,-6") //Output:  Error: negative numbers not allowed-1,-4,-6,
+// add("1,2,3") //Output: 6
+// add("//;\n1;2") //Output: 3
 
 
 
